5c77a6cc1f0ca22beb54fa70aea4e3fbee35a609
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-01-07 23:00:05 -0500

    Create dummy data and project

---
+++ splits/Images+Names+Colors.swift
@@ -0,0 +1,26 @@
+//
+//  Images+Names+Colors.swift
+//  splits
+//
+//  Created by Ricardo Herrera Petit on 1/7/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+//Images
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouqet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+let imageArray = [camera, city, buffalo, bouqet, stand, urban]
+
+//Names
+let namesArray = ["CAMERA", "CITY", "ANIMALS", "FLOWERS", "STANDS", "URBAN"]
+
+//Colors
+let colorsArray = [#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1),#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1),#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)]
+
