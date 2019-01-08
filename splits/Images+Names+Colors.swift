18c49bc10a8f81ff9051d973fa846c5552c2f128
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-01-07 23:10:14 -0500

    Completing the Master view and setting up the tableview

--- splits/Images+Names+Colors.swift
+++ splits/Images+Names+Colors.swift
@@ -22,5 +22,5 @@ let imageArray = [camera, city, buffalo, bouqet, stand, urban]
 let namesArray = ["CAMERA", "CITY", "ANIMALS", "FLOWERS", "STANDS", "URBAN"]
 
 //Colors
-let colorsArray = [#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1),#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1),#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)]
+let colorsArray = [#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1),#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1),#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1),#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)]
 
